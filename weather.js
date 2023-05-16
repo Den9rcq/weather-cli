c90b7084d70e08e853e0dc3b255b474464fec7de
Denis Bugreev <Den9.rcq@gmail.com>
2023-05-16 18:22:55 +0300

    call weather

--- weather.js
+++ weather.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
+import chalk from 'chalk'
 import { getArgs } from './helpers/args.js'
 import { printError, printSuccess, printHelp } from "./services/log.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+import { getWeather } from "./services/api.service.js";
 
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен')
+    return
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token)
+    printSuccess(`Токен ${chalk.green(token)} сохранён`)
+  } catch (e) {
+    printError(e.message)
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv)
@@ -14,10 +29,11 @@ const initCLI = () => {
   }
 
   if (args.t) {
-    // Сохранить токен
+    return saveToken(args.t)
   }
 
   // Вывести погоду
+  getWeather('moscow')
 }
 
 initCLI()
